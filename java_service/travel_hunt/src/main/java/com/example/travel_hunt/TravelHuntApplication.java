5bf6e9ec6c0db6172b30ea3de1f41176fc2ab823
mircea216 <75624674+mircea216@users.noreply.github.com>
2022-10-16 21:46:02 +0300

    Made setup for java microservice

---
+++ java_service/travel_hunt/src/main/java/com/example/travel_hunt/TravelHuntApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_hunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelHuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelHuntApplication.class, args);
+	}
+
+}
---
+++ java_service/travel_hunt/src/test/java/com/example/travel_hunt/TravelHuntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_hunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelHuntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

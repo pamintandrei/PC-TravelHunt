c663f1069d8c16ac48bbdf3471146756d14a5aa5
Daniel Morjan <daniel.morjan@infoworld.ro>
2022-11-07 00:08:32 +0200

    database insert

--- ts_service/src/addForm/index.ts
+++ ts_service/src/addForm/index.ts
@@ -1,17 +1,27 @@
 import { FormParser } from "./parser/FormParser";
 import { existsSync, readdirSync, readFileSync } from 'fs';
 
-const directoryPath="upload"
+const directoryPath = "upload"
 async function runAdd() {
 
-    const fileNames = readdirSync(directoryPath);
-    const parser=new FormParser();
+  const fileNames = readdirSync(directoryPath);
+  const parser = new FormParser();
 
-    for ( let fileName of fileNames) {
-        const details=parser.parseFile(fileName);
-        //await insert to database
-        console.log(details);
-      }
+  for (let fileName of fileNames) {
+    const details = parser.parseFile(fileName);
+    const url = "localhost:3000/Building"
+    const response = await fetch(url, {
+      method: 'post',
+      body: JSON.stringify(details),
+      headers: {
+        'accept': 'application/json',
+        'content-Type': 'application/json'
+      },
+    });
+    if(!response.ok){
+      throw new Error(' SERVER Error');
+    }
+  }
 }
 
-runAdd().then(_=> console.log("finished"));
+runAdd().then(_ => console.log("finished"));
